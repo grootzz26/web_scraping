5c27ac46d8f15343afbf89c9eafd52760fe8e44d
grootzz26 <sargunaraj26042000@gmail.com>
2023-09-06 22:57:26 +0530

    updated code for main image

--- website/forms.py
+++ website/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class AsinForm(forms.Form):
-    asin = forms.CharField(max_length=200, required=True)
-    path = forms.CharField(max_length=200, required=True)
+    asin = forms.CharField(widget=forms.Textarea, required=True)
+    path = forms.CharField(max_length=255, required=True)
